4bbb414d9f710290c328972038d68239b513aff8
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-03-24 22:08:32 +0000

    works, add buttons

--- js/simple-trvlr.js
+++ js/simple-trvlr.js
@@ -6,6 +6,8 @@ $(function(){
   wireDeck=function(deckName,deckLength){
    var slide=1;
    $('.'+deckName+' img').click(function(){
+      window.scrollTo(0, 1);
+
       $('.'+deckName+' .s'+slide).hide();
       slide++;
       if(slide>deckLength){
@@ -18,18 +20,25 @@ $(function(){
   wireDeck('screens',7);
   
   syncOrientation=function(){
+   window.scrollTo(0, 1);
     if(typeof(window.orientation)=="undefined") return;
-
-   var sel = (window.orientation%180==0)?"slides":"screens";
+   var isVert=window.orientation%180==0;
+   var sel = (isVert)?"slides":"screens";
    $('.slides,.screens').show();
    $('.'+sel).hide();
 
-   window.scrollTo(0, 1);
-
+   var targetH=0;
+   if(navigator.userAgent.match(/iphone/i)){
+      targetH=isVert?416:268;
+   }else{
+      targetH=$('body').height();
+   }
+   $('.slides img,.screens img').css({height:targetH});
   }
+
   window.onorientationchange=syncOrientation;
   syncOrientation();
-
+  $(window).resize(syncOrientation);
   fakeOrientation=function(deg){
     window.orientation=deg;
     syncOrientation();
