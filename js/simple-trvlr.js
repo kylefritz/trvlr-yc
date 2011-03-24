7ee12740cfce0229d7405de8053af1d63bdca926
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-03-24 22:54:10 +0000

    go with it

--- js/simple-trvlr.js
+++ js/simple-trvlr.js
@@ -5,19 +5,33 @@ $(function(){
 
   wireDeck=function(deckName,deckLength){
    var slide=1;
-   $('.'+deckName+' img').click(function(){
+   var deltSlide=function(d){
       window.scrollTo(0, 1);
 
       $('.'+deckName+' .s'+slide).hide();
-      slide++;
+      slide+=d;
       if(slide>deckLength){
         slide=1;
       }
+      if(slide<1){
+        slide=deckLength;
+      }
       $('.'+deckName+' .s'+slide).show();
-    }); 
+
+    }
+   /*
+     $('.'+deckName+' img').click(function(){
+      deltSlide(1);   
+      return false;
+     }); 
+     */
+     $('.'+deckName).click(function(evt){
+       deltSlide( evt.pageX<($(window).width()/2)?-1:+1);
+        $('.instruct').fadeOut();
+     });
   }
   wireDeck('slides',13);//its one indexed
-  wireDeck('screens',7);
+  wireDeck('screens',8);
   
   syncOrientation=function(){
    window.scrollTo(0, 1);
